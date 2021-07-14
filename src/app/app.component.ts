c100ad9cde1a0fd668b0813a1275cbef36ead4be
zigart <agustinzigart@gmail.com>
2021-07-14 19:18:36 -0300

    Sumatoria de dos numeros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  public operandoA:number = 0;
+  public operandoB: number = 0;
+  public resultado: number = 0;
+
+  obtenerValorA(event:Event){
+    this.operandoA = Number((<HTMLInputElement>event.target).value);
+  }
+
+  obtenerValorB(event:Event){
+    this.operandoB = Number((<HTMLInputElement>event.target).value);
+  }
+
+  sumar(){
+    this.resultado = this.operandoA + this.operandoB
+  }
 }
